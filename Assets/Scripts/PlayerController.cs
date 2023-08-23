66e6c8c44044a7b00322f9cf26c0bdf7a6411970
Kevin Song <songks712@gmail.com>
2023-08-23 02:39:43 -0400

    Player Movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,16 +4,22 @@ using UnityEngine;
 
 public class PlayerController: MonoBehaviour
 {
-    public float speed = 20;
+    // constants
+    public float speed = 0.5f;
     public float horizontalInput;
     public float forwardInput;
-    public float jumpForce;
-
-    public int totalJumps;
-    public int numJumps;
     private float jumpDelta = 0.15f;
     private float nextJump = 0.5f;
     private float jumpTime = 0.0f;
+    public float maxSpeed = 20.0f;
+
+    // movement
+    public float jumpForce;
+    public Vector2 turn;
+    public float sensitivity = 1.0f;
+    public int totalJumps;
+    public int numJumps;
+ 
 
     private float gravityModifier = 1.5f;
 
@@ -24,14 +30,21 @@ public class PlayerController: MonoBehaviour
     {
         rb = GetComponent<Rigidbody>();
         numJumps = totalJumps;
+        rb.maxDepenetrationVelocity = 20;
         Physics.gravity *= gravityModifier;
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        TurnHorizontal();
+        Jump();
         Move();
-        
+    }
+
+    private void FixedUpdate()
+    {
     }
 
     void OnCollisionEnter(Collision collision) {
@@ -41,15 +54,21 @@ public class PlayerController: MonoBehaviour
         }
     }
 
+    void ClampHorizontal() {
+        float newX = Mathf.Clamp(rb.velocity.x, -maxSpeed, maxSpeed);
+        float newZ = Mathf.Clamp(rb.velocity.z, -maxSpeed, maxSpeed);
+        rb.velocity = new Vector3(newX, rb.velocity.y, newZ);
+    }
     void Move() {
-        jumpTime += Time.deltaTime;
-
         horizontalInput = Input.GetAxis("Horizontal");
         forwardInput = Input.GetAxis("Vertical");
-        transform.Translate(Vector3.right * Time.deltaTime * speed * horizontalInput);
-        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
-        // transform.position = transform.position + new Vector3(0, 0, 1);
+        rb.AddForce(transform.forward * forwardInput * speed, ForceMode.Impulse);
+        rb.AddForce(transform.right * horizontalInput * speed, ForceMode.Impulse);
+        ClampHorizontal();
+    }
 
+    void Jump() {
+        jumpTime += Time.deltaTime;
         if (Input.GetKeyDown(KeyCode.Space) && numJumps > 0 && jumpTime > nextJump)
         {
             nextJump = jumpTime + jumpDelta;
@@ -59,5 +78,10 @@ public class PlayerController: MonoBehaviour
             nextJump = nextJump - jumpTime;
             jumpTime = 0.0f;
         }
+    }
+
+    void TurnHorizontal() {
+        turn.x += Input.GetAxis("Mouse X") * sensitivity;
+        transform.localRotation = Quaternion.Euler(0, turn.x, 0);
     }
 }
